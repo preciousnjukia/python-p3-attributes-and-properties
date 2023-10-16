4522a8520425d727f5b9d94373bf8b3f2e082cff
preciousnjukia <preciousnjukia@gmail.com>
2023-10-16 12:17:00 +0300

    Assignment done

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,38 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
-    pass
+    APPROVED_BREEDS = ["Mastiff","Chihuahua","Corgi","Shar Pei","Beagle","French Bulldog","Pug","Pointer"]
+
+    def __init__(self, name, breed):
+        self._name = None
+        self._breed = None
+        self.name = name
+        self.breed = breed
+
+    def name(self):
+        return self._name
+
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    def breed(self):
+        return self._breed
+
+    def breed(self, value):
+        if value in self.APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in the list of approved breeds.")
+
+
+fido = Dog("Fido", "Pug")
+print(fido.breed)  
+
+fido.breed = "xyz"
+#Output:Name must be a string under 25 characters.
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,34 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    APPROVED_JOBS = ["Admin","Customer Service","Human Resources","ITC","Production","Legal","Finance","Sales","General Management","Research & Development","Marketing","Purchasing"]
+    def __init__(self, name, job):
+        self._name = None
+        self._job = None
+        self.name = name
+        self.job = job
+
+    def name(self):
+        return self._name
+
+    def name(self, value):
+        if isinstance(value, str) and len(value) < 25:
+            self._name = value.title()
+        else:
+            print("Name must be a string under 25 characters.")
+
+    def job(self):
+        return self._job
+
+    def job(self, value):
+        if value in self.APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in the list of approved jobs.")
+
+
+guido = Person("Guido Van Rossum", "ITC")
+print(guido.job) 
+
+guido.job = "Waiter"
+# Output: Job must be in the list of approved jobs.
